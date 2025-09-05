b1b3db48bace78308e1983fe1fffddcb19bbd8d1
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-05 12:48:53 +0100

    feat: add if/else and for loop examples

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, cuddly-goggles!")
+	myname := "david"
+	myage := 99
+    	fmt.Println("Hello, cuddly-goggles! My name is", myname, "and I am", myage, "years old.")
+	if myage > 60 {
+        	fmt.Println("You are in the golden years!")
+    	} else {
+        	fmt.Println("Still a spring chicken!")
+    	}
+	if myname == "david" {
+        	fmt.Println("Hello, you legend!")
+    	} else {
+        	fmt.Println("Nice to meet you!")	
+		}	
+	for i := 3; i > 0; i-- {
+			fmt.Println("T-minus:", i, "seconds")
+	}
+	fmt.Println("Liftoff!")
+
 }
