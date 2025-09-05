be4c5685a3f34e0d46cd6723452d7c7f4aac9ca1
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-05 15:56:00 +0100

    feat: Add Loops, if, else and slices

--- main.go
+++ main.go
@@ -2,23 +2,41 @@ package main
 
 import "fmt"
 
-func main() {
-	myname := "david"
-	myage := 99
-    	fmt.Println("Hello, cuddly-goggles! My name is", myname, "and I am", myage, "years old.")
-	if myage > 60 {
-        	fmt.Println("You are in the golden years!")
-    	} else {
-        	fmt.Println("Still a spring chicken!")
-    	}
-	if myname == "david" {
-        	fmt.Println("Hello, you legend!")
-    	} else {
-        	fmt.Println("Nice to meet you!")	
-		}	
-	for i := 3; i > 0; i-- {
-			fmt.Println("T-minus:", i, "seconds")
+func displayGreeting(name string, age int) {
+	fmt.Println("Hello, cuddly-goggles! My name is", name, "and I am", age, "years old.")
+
+	if name == "david" {
+		fmt.Println("Hello, you legend!")
+	} else {
+		fmt.Println("Nice to meet you!")
+	}
+}
+func performCountdown(start int) {
+	fmt.Println("\n--- Starting Countdown ---")
+	for i := start; i > 0; i-- {
+		fmt.Println("T-minus:", i, "seconds")
 	}
 	fmt.Println("Liftoff!")
-
+}	
+func checkAge(age int) {
+	if age < 18 {
+		fmt.Println("You are a minor.")
+	} else {
+		fmt.Println("Age check complete.")
+	}
 }
+
+func main() {
+	myName := "david"
+	myAge := 21
+	hobbies := []string{"Reading", "Hiking", "Coding"}
+	displayGreeting(myName, myAge)
+	checkAge(myAge)
+	performCountdown(myAge)
+	for index, hobby := range hobbies {
+    	fmt.Printf("Hobby #%d is %s\n", index, hobby)
+	}
+	
+	fmt.Println("\n--- End of Program ---") 
+}
+	
