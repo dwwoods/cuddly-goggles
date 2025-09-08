17b293518e1773696eb987559dea89ac66bf08f2
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-08 18:03:14 +0100

    feat: learn maps

--- main.go
+++ main.go
@@ -2,43 +2,15 @@ package main
 
 import "fmt"
 
-func displayGreeting(name string, age int) {
-	fmt.Println("Hello, cuddly-goggles! My name is", name, "and I am", age, "years old.")
-
-	if name == "david" {
-		fmt.Println("Hello, you legend!")
-	} else {
-		fmt.Println("Nice to meet you!")
-	}
-}
-func performCountdown(start int) {
-	fmt.Println("\n--- Starting Countdown ---")
-	for i := start; i > 0; i-- {
-		fmt.Println("T-minus:", i, "seconds")
-	}
-	fmt.Println("Liftoff!")
-}	
-func checkAge(age int) {
-	if age < 18 {
-		fmt.Println("You are a minor.")
-	} else {
-		fmt.Println("Age check complete.")
-	}
-}
 
 func main() {
-	myName := "david"
-	myAge := 21
-	hobbies := []string{"Reading", "Hiking"}
-	displayGreeting(myName, myAge)
-	checkAge(myAge)
-	performCountdown(myAge)
-	hobbies = append(hobbies, "Coding", "Gaming")
-    fmt.Println("Updated hobbies:", hobbies)
-	for index, hobby := range hobbies {
-    	fmt.Printf("Hobby #%d is %s\n", index, hobby)
-	}
+	// create the map
+	userProfile := map[string]string{
+		"name":    "david",
+		"favoriteColour": "green",
+		"city":    "Imaginaria",
+	}		
+	fmt.Println("The user's city is:", userProfile["city"])
 	
-	fmt.Println("\n--- End of Program ---") 
+fmt.Println("\n--- End of Program ---") 
 }
-	
